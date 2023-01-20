39f9108c65cad6f13073cdc2bd2fc501695ee07d
atrox3d <atrox3d@gmail.com>
2023-01-20 11:30:32 +0100

    fix file format, it was mp3 (default) with wav extension

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ print(f'{type(original)=}, {original=}')
 
 # reverse audiofile
 reversed = original.reverse()
-reversed.export('out/reversed.wav')
+reversed.export('out/reversed.wav', format='wav')
 
 # get clip of audiofile
 # slicing in milliseconds! how cool is that?
 first_2_minutes = original[0:2000]
-first_2_minutes.export('out/first_2_minutes.wav')
+first_2_minutes.export('out/first_2_minutes.wav', format='wav')
 
 # get len of audio in milliseconds
 print(f'{len(original)=} ms')
@@ -29,23 +29,23 @@ print(f'{len(first_2_minutes)=} ms')
 
 # append reversed to original
 merged = original + reversed
-merged.export('out/merged.wav')
+merged.export('out/merged.wav', format='wav')
 print(f'{len(merged)=} ms')
 
 # create 1000 ms of silence
 silence = AudioSegment.silent(1000)
-silence.export('out/silence.wav')
+silence.export('out/silence.wav', format='wav')
 print(f'{len(silence)=} ms')
 
 # repeat clip n times
 double_original = original * 2
-double_original.export('out/double_original.wav')
+double_original.export('out/double_original.wav', format='wav')
 print(f'{len(double_original)=} ms')
 
 # increase volume by 15 DB
 double_original_increased = double_original + 15
 
 doubleoriginalincreased_silence_reversed = double_original_increased + silence + reversed
-doubleoriginalincreased_silence_reversed.export('out/doubleoriginalincreased-silence-reversed.wav')
+doubleoriginalincreased_silence_reversed.export('out/doubleoriginalincreased-silence-reversed.wav', format='wav')
 print(f'{len(doubleoriginalincreased_silence_reversed)=} ms')
 
